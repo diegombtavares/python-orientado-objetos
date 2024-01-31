ca37d8af5f2f4f7c0b3a1f72969cc64ab0488da6
Diego Machado <diego.mborges08@gmail.com>
2024-01-31 14:25:00 -0300

    feat: add avaliacao

--- orientacao-objetos/modelos/restaurante.py
+++ orientacao-objetos/modelos/restaurante.py
@@ -1,3 +1,4 @@
+from modelos.avaliacao import Avaliacao
 class Restaurante:
     restaurantes = []
     # Metodo construtor - definição de valores
@@ -5,6 +6,7 @@ class Restaurante:
         self.nome = nome.title()
         self.categoria = categoria.upper()
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -12,9 +14,10 @@ class Restaurante:
     
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
+        print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Avaliações".ljust(25)} | {"Status"}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
+
 
     @property
     def ativo(self):
@@ -22,3 +25,18 @@ class Restaurante:
 
     def alternar_estado(self):
         self._ativo = not self._ativo
+
+    def receber_avaliacao(self, cliente, nota):
+        if 0 < nota <= 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
+    
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return '-'
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
