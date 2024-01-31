8f0c7450632d766b5c96cec64cd11247adc7196c
Diego Machado <diego.mborges08@gmail.com>
2024-01-31 14:25:17 -0300

    feat: add avaliacao

--- orientacao-objetos/app.py
+++ orientacao-objetos/app.py
@@ -1,10 +1,8 @@
 from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante('praça', 'gourmet')
-restaurante_mexicano = Restaurante('mexican food', 'mexicana')
-restaurante__japones = Restaurante('japa', 'japones')
-
-restaurante_mexicano.alternar_estado()
+restaurante_praca.receber_avaliacao('Gui', 10)
+restaurante_praca.receber_avaliacao('Lais', 1)
 
 def main():
     Restaurante.listar_restaurantes()
